4618b5355a287b2e0ad32fbba0fa18c6f017564d
Tevin-Mugairwa <tevin.mugairwa@student.moringaschool.com>
2022-08-15 11:36:24 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31;
+let num2 =2;
+let multiply=62;
+num1*num2;
+
+let random=1;
+
+let num3 = 24;
+let num4 = 5;
+let mod =4;
+num3 % num4;
+
+let max =20;
+
+
+
 
 // Write your code here
